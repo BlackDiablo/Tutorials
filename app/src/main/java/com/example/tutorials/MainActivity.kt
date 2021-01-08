221a6e19b1a8c8a5ef026c9f7393ab55d2314018
voroncovilya15 <voroncovilya15@gmail.com>
2021-01-09 01:08:08 +0600

    Update Core Code

--- app/src/main/java/com/example/tutorials/MainActivity.kt
+++ app/src/main/java/com/example/tutorials/MainActivity.kt
@@ -2,10 +2,12 @@ package com.example.tutorials
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.tutorials.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
     }
 }

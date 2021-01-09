6e7ecd3fbd9e835ea9072ae2027506de71d72148
voroncovilya15 <voroncovilya15@gmail.com>
2021-01-09 10:36:34 +0600

    This full Lifecycle of Activity

--- app/src/main/java/com/example/tutorials/MainActivity.kt
+++ app/src/main/java/com/example/tutorials/MainActivity.kt
@@ -10,4 +10,35 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
     }
+
+    override fun onStart() {
+        super.onStart()
+        println("Activity_onStart")
+
+    }
+
+    override fun onResume() {
+        super.onResume()
+        println("Activity_onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        println("Activity_OnPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        println("Activity_onStop")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        println("Activity_onRestart")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        println("Activity_onDestroy")
+    }
 }

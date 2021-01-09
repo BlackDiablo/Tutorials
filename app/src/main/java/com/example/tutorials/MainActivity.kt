3d4e6a06932237e5479e4aec80248c721ae76997
voroncovilya15 <voroncovilya15@gmail.com>
2021-01-09 13:33:32 +0600

    Example of using Liner Layout

--- app/src/main/java/com/example/tutorials/MainActivity.kt
+++ app/src/main/java/com/example/tutorials/MainActivity.kt
@@ -9,36 +9,10 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+        binding.run {
+
+        }
     }
 
-    override fun onStart() {
-        super.onStart()
-        println("Activity_onStart")
 
-    }
-
-    override fun onResume() {
-        super.onResume()
-        println("Activity_onResume")
-    }
-
-    override fun onPause() {
-        super.onPause()
-        println("Activity_OnPause")
-    }
-
-    override fun onStop() {
-        super.onStop()
-        println("Activity_onStop")
-    }
-
-    override fun onRestart() {
-        super.onRestart()
-        println("Activity_onRestart")
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        println("Activity_onDestroy")
-    }
 }
